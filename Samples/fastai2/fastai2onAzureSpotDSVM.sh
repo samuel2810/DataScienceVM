9a7779652fc94560d95ed4c2add7864d5788d612
samuel2810 <44728241+samuel2810@users.noreply.github.com>
2021-10-02 19:54:37 +0800

    Update fastai2onAzureSpotDSVM.sh

--- Samples/fastai2/fastai2onAzureSpotDSVM.sh
+++ Samples/fastai2/fastai2onAzureSpotDSVM.sh
@@ -18,7 +18,7 @@ az group create --name $vmname -l $vmzone
 echo "Creating Azure Data Science VM $vmname..."
 # You can change the size parameter if you want something other than NVidia K80 GPU instance. 
 # You can find Azure size labels by running Azure CLI command in Cloud Shell "az vm list-sizes -l westus2 -o table"  
-az vm create --name $vmname -g $vmname --image microsoft-dsvm:ubuntu-1804:1804:latest  --priority Low --size Standard_NC6 --eviction-policy Deallocate --storage-sku StandardSSD_LRS --admin-user fastuser --admin-password $password
+az vm create --name $vmname -g $vmname --image microsoft-dsvm:ubuntu-1804:1804:latest  --priority Low --size Standard_NC8as_T4_v3 --eviction-policy Deallocate --storage-sku StandardSSD_LRS --admin-user fastuser --admin-password $password
 az vm open-port --name $vmname -g $vmname --port 8000
 
 echo "Installing  fastai v2 and notebooks..."
